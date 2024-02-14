193f7c005e4d163f8f022048c0d1f037ad73c53f
flavio <flavioumberto1@gmail.com>
2024-02-14 18:26:42 +0200

    commit2-token

--- controler/controler_post_tweet.js
+++ controler/controler_post_tweet.js
@@ -1,10 +1,8 @@
 const Post= require("../controler/controler_tweets.js")
 
 const post_tweet=(req, res) => {
-
-    console.log(req.body);
     Post.push(req.body)
-    res.send(Post)
+    res.send("poste ajouté avec succès")
     }
     
 module.exports= post_tweet
